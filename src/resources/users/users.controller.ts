d60ca96bbb291f38c8ade26fea629c7d3370fa42
Roman <ismagilov771@gmail.com>
2022-11-09 00:41:41 +0300

    user controller code style fixes

--- src/resources/users/users.controller.ts
+++ src/resources/users/users.controller.ts
@@ -16,27 +16,30 @@ export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post('signup')
-  create(@Body() createUserDto: CreateUserDto) {
+  public create(@Body() createUserDto: CreateUserDto): Promise<any> {
     return this.usersService.create(createUserDto);
   }
 
   @Get()
-  findAll() {
+  public findAll(): Promise<any> {
     return this.usersService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.usersService.findOne(+id);
+  public findOne(@Param('id') id: string): Promise<any> {
+    return this.usersService.findOne(parseInt(id));
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.usersService.update(+id, updateUserDto);
+  public update(
+    @Param('id') id: string,
+    @Body() updateUserDto: UpdateUserDto,
+  ): Promise<any> {
+    return this.usersService.update(parseInt(id), updateUserDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.usersService.remove(+id);
+  public remove(@Param('id') id: string): Promise<any> {
+    return this.usersService.remove(parseInt(id));
   }
 }
